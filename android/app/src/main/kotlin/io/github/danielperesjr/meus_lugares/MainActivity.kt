3d391f26d39ff13640410742be265a02ab33bda9
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-12-10 20:25:22 -0300

    initial commit

---
+++ android/app/src/main/kotlin/io/github/danielperesjr/meus_lugares/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.danielperesjr.meus_lugares
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:meus_lugares/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Meus Lugares",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
