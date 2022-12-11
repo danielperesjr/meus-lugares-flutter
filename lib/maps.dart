e90f32face0be5f0baa9579cadb867144c8b9a5f
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-12-11 20:03:52 -0300

    feat: implements move camera & location listener methods

--- lib/maps.dart
+++ lib/maps.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget {
@@ -12,22 +13,48 @@ class Maps extends StatefulWidget {
 class _MapsState extends State<Maps> {
   Completer<GoogleMapController> _controller = Completer();
   Set<Marker> _markers = {};
+  CameraPosition _cameraPosition =
+      CameraPosition(target: LatLng(-23.562436, -46.655005), zoom: 18.0);
 
   void _onMapCreated(GoogleMapController googleMapController) {
     _controller.complete(googleMapController);
   }
 
-  void _displayBookmark(LatLng latLng){
+  void _displayBookmark(LatLng latLng) {
     Marker marker = Marker(
       markerId: MarkerId("marker-${latLng.latitude}-${latLng.longitude}"),
       position: latLng,
-      infoWindow: InfoWindow(
-        title: "Marker"
-      ),
+      infoWindow: InfoWindow(title: "Marker"),
     );
     setState(() {
       _markers.add(marker);
     });
+  }
+
+  Future<void> _moveCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(_cameraPosition),
+    );
+  }
+
+  void _addLocationListener() {
+    var geolocator =
+        Geolocator.getPositionStream().listen((Position position) {
+          setState(() {
+            _cameraPosition = CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18.0
+            );
+            _moveCamera();
+          });
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _addLocationListener();
   }
 
   @override
@@ -40,12 +67,10 @@ class _MapsState extends State<Maps> {
         child: GoogleMap(
           markers: _markers,
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-23.562436, -46.655005),
-            zoom: 18.0
-          ),
+          initialCameraPosition: _cameraPosition,
           onMapCreated: _onMapCreated,
           onLongPress: _displayBookmark,
+
         ),
       ),
     );
